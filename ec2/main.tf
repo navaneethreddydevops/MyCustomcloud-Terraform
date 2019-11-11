22b71c63eabac054488439f5859bc6b246c20e36
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2019-11-10 20:15:00 -0600

    adding the profile

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,11 +1,13 @@
 provider "aws" {
-    "region" = "us-east-1"
+  region                  = "us-east-1"
+  shared_credentials_file = "/Users/navaneethreddy/.aws/credentials"
+  profile                 = "default"
 }
 resource "aws_instance" "ec2" {
-    instance_type     = "t2.micro"
-    availability_zone = "us-east-1a"
-    ami               = "ami-00dc79254d0461090"
-    user_data         = <<-EOF
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  ami               = "ami-00dc79254d0461090"
+  user_data         = <<-EOF
                 #!/bin/bash
                 sudo service apache2 start
                 EOF 
