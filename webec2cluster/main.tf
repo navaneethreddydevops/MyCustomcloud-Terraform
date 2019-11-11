6c6c51b04507cc43aae57fd2b10316b99d9f9fb3
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2019-11-11 15:22:20 -0600

    updated the sg

--- webec2cluster/main.tf
+++ webec2cluster/main.tf
@@ -4,9 +4,9 @@ provider "aws" {
   profile                 = "default"
 }
 resource "aws_launch_configuration" "launchtemplate" {
-  imageid            = "ami-00dc79254d0461090"
+  image_id            = "ami-00dc79254d0461090"
   instance_type      = "r4.xlarge"
-  aws_security_group = [aws_security_group.securitygroup.id]
+  security_groups = [aws_security_group.securitygroup.id]
   user_data          = <<-EOF
                 #!/bin/bash
                 echo "Hello World" > index.html
@@ -19,18 +19,18 @@ resource "aws_security_group" "securitygroup" {
   description = "Allow TLS inbound traffic"
   ingress {
     # TLS (change to whatever ports you need)
-    from_port = 443
-    to_port   = 443
-    protocol  = "-1"
+    from_port = var.server_port
+    to_port   = var.server_port
+    protocol  = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = ["172.31.0.0/16"] # add a CIDR block here
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
