4983ccace1bf574a6fd1a6db2aaa0fc0bdf8e20e
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2019-11-10 20:10:24 -0600

    ADDING EC2

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-
-  
+    "region" = "us-east-1"
 }
 resource "aws_instance" "ec2" {
-  
+    instance_type     = "t2.micro"
+    availability_zone = "us-east-1a"
+    ami               = "ami-00dc79254d0461090"
+    user_data         = <<-EOF
+                #!/bin/bash
+                sudo service apache2 start
+                EOF 
 }
-
