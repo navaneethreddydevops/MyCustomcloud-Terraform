adc107b315455590bbe5bde507f50f6e24ec0cc4
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2019-12-02 22:55:05 -0600

    adding the syntax updates

--- ec2/main.tf
+++ ec2/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2" {
     instance_type          = "t2.micro"
     availability_zone      = "us-east-1a"
     ami                    = "ami-00dc79254d0461090"
-    vpc_security_group_ids = [aws_security_group.allow_web_traffic.id]
+    vpc_security_group_ids = "${aws_security_group.allow_web_traffic.id}"
     tags = {
       Name = "mycustomcloud-ec2"
 
@@ -37,7 +37,7 @@ resource "aws_security_group" "allow_web_traffic" {
     protocol  = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["0.0.0.0/0"] # add a CIDR block here
+    cidr_blocks = ["10.0.1.0/24"] # add a CIDR block here
   }
   ingress {
     from_port = 8090
@@ -45,7 +45,7 @@ resource "aws_security_group" "allow_web_traffic" {
     protocol  = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.1.0/24"]
 
   }
   
