2a7d9c66c7811f92bab3d91d3a8d2ca9419ede90
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2019-12-08 10:29:58 -0600

    adding the vpc dns support

--- Multi-Tier/terraform/main.tf
+++ Multi-Tier/terraform/main.tf
@@ -4,8 +4,9 @@ provider "aws" {
   profile                 = "default"
 }
 resource "aws_vpc" "multitier_vpc" {
-  cidr_block       = "${var.vpc_cidr}"
-  instance_tenancy = "default"
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name                = "Multitier-VPC"
     Ownercontact        = "navaneethreddydevops@gmail.com"
@@ -35,10 +36,9 @@ resource "aws_subnet" "PUBLIC_SUBNET_2" {
   }
 }
 resource "aws_subnet" "PRIVATE_SUBNET_1" {
-  vpc_id                  = "${aws_vpc.multitier_vpc.id}"
-  cidr_block              = "${var.PRIVATE_SUBNET_CIDR_1}"
-  availability_zone       = "us-west-2a"
-  map_public_ip_on_launch = "true"
+  vpc_id            = "${aws_vpc.multitier_vpc.id}"
+  cidr_block        = "${var.PRIVATE_SUBNET_CIDR_1}"
+  availability_zone = "us-west-2a"
   tags = {
     Name                = "PRIVATE_SUBNET_1"
     Ownercontact        = "navaneethreddydevops@gmail.com"
@@ -46,10 +46,9 @@ resource "aws_subnet" "PRIVATE_SUBNET_1" {
   }
 }
 resource "aws_subnet" "PRIVATE_SUBNET_2" {
-  vpc_id                  = "${aws_vpc.multitier_vpc.id}"
-  cidr_block              = "${var.PRIVATE_SUBNET_CIDR_2}"
-  availability_zone       = "us-west-2b"
-  map_public_ip_on_launch = "true"
+  vpc_id            = "${aws_vpc.multitier_vpc.id}"
+  cidr_block        = "${var.PRIVATE_SUBNET_CIDR_2}"
+  availability_zone = "us-west-2b"
   tags = {
     Name                = "PRIVATE_SUBNET_2"
     Ownercontact        = "navaneethreddydevops@gmail.com"
